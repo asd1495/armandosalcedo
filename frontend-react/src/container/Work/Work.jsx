0fe017988466db22bac0cce69c182020b200f53d
asd1495 <armando.salcedo95@gmail.com>
2022-11-07 21:32:01 -0600

    added index and filter for work section

--- frontend-react/src/container/Work/Work.jsx
+++ frontend-react/src/container/Work/Work.jsx
@@ -6,15 +6,30 @@ import { urlFor, client } from '../../client'
 import './Work.scss'
 
 const Work = () => {
+
+    const [activeFilter, setActiveFilter] = useState('All')
+
+    const handleWorkFilter = (item) => {
+
+    }
+
     return (
         <>
             <h2 className='head-text'>
-                I know that
-                <span> Good Development </span>
-                <br />
-                means
-                <span> Good Business</span>
+                My Creative
+                <span> Portfolio </span>
             </h2>
+            <div className='app__work-filter'>
+                {[ 'UI/UX', 'Web App', 'Mobile App', 'React JS', 'All' ].map((item, index) => (
+                    <div
+                    key={index}
+                    onClick={() => handleWorkFilter(item)}
+                    className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
+                    >
+                        {item}
+                    </div>
+                ))}
+            </div>
         </>
     )
 }
